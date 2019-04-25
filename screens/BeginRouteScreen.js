12c12578a91bdce66e00ce5fdfdd2a47d7e982a5
Bradley Frysinger <bradley.frysinger@kroger.com>
2019-04-25 19:54:01 -0400

    Added non-functional address list and confirm button, left comments and TODO's.

--- screens/BeginRouteScreen.js
+++ screens/BeginRouteScreen.js
@@ -1,21 +1,157 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { Button } from 'react-native-elements';
+import {Platform, ScrollView, StyleSheet, Text, View} from 'react-native';
+import { ListItem, Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { MonoText } from "../components/StyledText";
 
 export default class BeginRouteScreen extends React.Component {
 
+
+
   render() {
+
+    //TODO Implement confirm route button function
+    //TODO Retrieve actual address list before renders
+    /*
+    This array can be expanded to include as many elements as needed.
+    The easiest solution to display addresses is to write a function to
+    retrieve them from the selected route and populate the array
+    before we render the page.
+
+    Google "react native elements ListItem" for more info.
+     */
+    const addressList = [
+      {
+        addressLineOne: 'ADDRESS ONE',
+        addressLineTwo: 'SECONDLINE ONE'
+      },
+      {
+        addressLineOne: 'ADDRESS TWO',
+        addressLineTwo: 'SECONDLINE TWO'
+      }];
+
     return (
-      <ScrollView style={styles.container}>
-      </ScrollView>
+        <View style={styles.container}>
+          <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+            <View style={styles.welcomeContainer}></View>
+
+            <View style={styles.getStartedContainer}>
+              <Text style={styles.getStartedText}>ROUTENAMEHERE</Text>
+
+              <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+                <MonoText style={styles.codeHighlightText}>Confirm Addresses</MonoText>
+              </View>
+            </View>
+
+            <View>
+              {
+                addressList.map((item, i) => (
+                    <ListItem
+                        key={i}
+                        title={item.addressLineOne}
+                        subtitle={item.addressLineTwo}
+                    />
+                ))
+              }
+            </View>
+
+            <View>
+              <Button title="Confirm Route"/>
+            </View>
+
+          </ScrollView>
+        </View>
     );
   }
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff',
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 40,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 84,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
   },
 });
